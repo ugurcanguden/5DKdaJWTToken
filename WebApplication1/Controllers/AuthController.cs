d43841ea149980658a0f99daa522a7ab25e90981
uğurcan güden <ugurcanguden@hotmail.com>
2022-12-18 12:03:56 +0300

    Add project files.

---
+++ WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,99 @@
+﻿using Guden.Lib.Core;
+using Guden.Lib.Bll; 
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthController : Controller
+    { 
+            private IAuthService _authService; private ICore_MenuService _menuService;
+            public AuthController(IAuthService authService, ICore_MenuService core_MenuService)
+            {
+                _authService = authService;
+                _menuService = core_MenuService;
+            }
+
+            [AllowAnonymous]
+            [HttpPost("Login")]
+            public IActionResult Login([FromBody] UserForLoginDto userForLoginDto)
+            {
+                var userToLogin = _authService.Login(userForLoginDto);
+                if (!userToLogin.Success)
+                {
+                    return Ok(userToLogin);
+                }
+
+                var result = _authService.CreateAccessToken(userToLogin.Data);
+                if (result.Success)
+                {
+
+                    return Ok(result);
+                }
+
+                return Ok(result);
+
+            }
+            [HttpPost("Register")]
+            public ActionResult Register([FromBody] UserForRegisterDto userForRegisterDto)
+            {
+                //userExists
+                var userExists = _authService.UserExists(userForRegisterDto.Email, null);
+                if (!userExists.Success)
+                {
+                    return Ok(userExists);
+                }
+                //user added
+                var registerResult = _authService.Register(userForRegisterDto);
+
+                if (registerResult.Success)
+                {
+                    return Ok(registerResult);
+                }
+
+                return Ok(registerResult);
+            }
+            [HttpPost("UpdateUser")]
+            public ActionResult UpdateUser([FromBody] UserForRegisterDto userForRegisterDto)
+            {
+                //user Exists
+                var userExists = _authService.UserExists(userForRegisterDto.Email, userForRegisterDto.Id);
+                if (!userExists.Success)
+                {
+                    return Ok(userExists);
+                }
+                //user added
+                var registerResult = _authService.UpdateUser(userForRegisterDto);
+
+                if (registerResult.Success)
+                {
+                    return Ok(registerResult);
+                }
+
+                return Ok(registerResult);
+            }
+            [HttpGet("GetCore_MenuListByUserId")]
+            [Authorize]
+            public IActionResult GetCore_MenuListByUserId()
+            {
+                int userId = Token.GetUserId(User);
+
+                var result = _menuService.GetMenusByUser(userId);
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return Ok(result);
+                }
+            }
+            [HttpPost("RefreshToken")]
+            public ActionResult RefreshToken([FromBody] TokenDto token)
+            {
+                int userId = Token.GetUserId(User);
+                return Ok(_authService.CreateRefreshToken(token.RefreshToken));
+            }
+        }
+    }
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,60 @@
+using Guden.lib.Bll;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#region 
+ServiceCollectionModule.SetAuthenticationModuleRegister(builder);
+#endregion App Settings
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test01", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
